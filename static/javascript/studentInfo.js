720274ec42414a7128737d8d9bc09fc9b12666e3
yalin1997 <godjj1997@gmail.com>
2019-10-17 21:15:06 +0800

    換輪播

---
+++ static/javascript/studentInfo.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    $(".flexslider").flexslider({
+        slideshowSpeed: 5000, //展示时间间隔ms
+        animationSpeed: 500, //滚动时间ms
+        touch: true //是否支持触屏滑动
+    });
+});
