e2f5c7e08b089174a4729655ce18986e59c2a4e6
stanley_chen <stanley_chen@gss.com.tw>
2019-10-21 00:42:36 +0800

    male celery

--- recognitionApi.py
+++ recognitionApi.py
@@ -58,6 +58,19 @@ app.config.update(
     CELERY_BROKER_URL='redis://localhost:6379',
     CELERY_RESULT_BACKEND='redis://localhost:6379'
 )
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
 celery = make_celery(app)
 
 #savePath = '/home/nknu/文件/faceRecog/embDir'
@@ -79,17 +92,7 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 login_manager.remember_cookie_duration=timedelta(days=1)
 login_manager.init_app(app)
 
-def make_celery(app):
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
+
 
 # 儲存檔案
 def saveUploadFile(uploadedFile):
