dd5e60fbc78ce76f8c341e0823d68aca14acc3e9
stanley_chen <stanley_chen@gss.com.tw>
2019-10-21 00:58:50 +0800

    celery

--- recognitionApi.py
+++ recognitionApi.py
@@ -1,5 +1,5 @@
 import flask # api 依賴
-from celery import Celery
+import celery
 from flask import request,jsonify,render_template,redirect,send_from_directory,g,session,flash
 from flaskClass.loginForm import EmailPasswordForm
 from flaskClass.uploadForm import uploadForm,videoEditForm,userUploadForm
@@ -59,19 +59,9 @@ app.config.update(
     CELERY_RESULT_BACKEND='redis://localhost:6379'
 )
 
-def make_celery(app):
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
 
-celery = make_celery(app)
+
+celery = celery.make_celery(app)
 
 #savePath = '/home/nknu/文件/faceRecog/embDir'
 
