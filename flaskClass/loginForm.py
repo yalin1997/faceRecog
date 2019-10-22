bd51ca7c8d27840cf3700490d6f82bceed4a2f40
PUPU <majikkutori@gmail.com>
2019-10-22 15:40:40 +0800

    Update loginForm.py

--- flaskClass/loginForm.py
+++ flaskClass/loginForm.py
@@ -5,8 +5,7 @@ from wtforms.validators import DataRequired, Email
 class EmailPasswordForm(FlaskForm):
     account = StringField('account', validators=[DataRequired()] , render_kw={'class': 'form-control'})
     password = PasswordField('Password', validators=[DataRequired()] , render_kw={'class': 'form-control'})
-
-    submit = SubmitField('Submit')
+    submit = SubmitField('Submit' , render_kw={'class': 'btn btn-primary'})
 
     def validate_account(self, value):
         # todo
