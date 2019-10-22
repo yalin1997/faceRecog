bfc15659785cb7aeb26753e89a4e4a5a053d5875
stanley_chen <stanley_chen@gss.com.tw>
2019-10-22 15:38:00 +0800

    家class

--- flaskClass/loginForm.py
+++ flaskClass/loginForm.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField,SubmitField
 from wtforms.validators import DataRequired, Email
 
 class EmailPasswordForm(FlaskForm):
-    account = StringField('account', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    account = StringField('account', validators=[DataRequired()] , render_kw={'class': 'form-control'})
+    password = PasswordField('Password', validators=[DataRequired()] , render_kw={'class': 'form-control'})
 
     submit = SubmitField('Submit')
 
