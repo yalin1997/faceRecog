46c28d52dde57b4d03c251e63910e13b2f0dbdb7
yalin1997 <godjj1997@gmail.com>
2019-10-24 17:59:12 +0800

    'class': 'form-control'

--- flaskClass/PictureEditForm.py
+++ flaskClass/PictureEditForm.py
@@ -3,6 +3,6 @@ from wtforms import SubmitField,StringField,SelectField
 
 class pictureEditForm(FlaskForm):
     lastName = StringField('lastName', validators=[],render_kw={'placeholder': u'姓'} , render_kw={'class': 'form-control'})
-    firstName = StringField('firstName', validators=[],render_kw={'placeholder': u'名'} , render_kw={'class': 'form-control'})
+    firstName = StringField('firstName', validators=[],render_kw={'placeholder': u'名' , 'class': 'form-control'})
     id = StringField('id')
 
