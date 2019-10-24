3199445966be3b1528d3e595532d5ae563ebf532
PUPU <majikkutori@gmail.com>
2019-10-24 18:03:09 +0800

    Update filterForm.py

--- flaskClass/filterForm.py
+++ flaskClass/filterForm.py
@@ -4,8 +4,8 @@ from wtforms import SubmitField,StringField,SelectField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import Optional
 class videoFilter(FlaskForm):
-    lastName = StringField('lastName', validators=[Optional()],render_kw={'placeholder': u'姓'})
-    firstName = StringField('firstName', validators=[Optional()],render_kw={'placeholder': u'名'})
+    lastName = StringField('lastName', validators=[Optional()],render_kw={'placeholder': u'姓' , 'class': 'form-control'})
+    firstName = StringField('firstName', validators=[Optional()],render_kw={'placeholder': u'名' , 'class': 'form-control'})
     classNo = SelectField(
         '節數',
         choices=[(1 , '第一節'),
@@ -17,6 +17,7 @@ class videoFilter(FlaskForm):
         (7 , '第七節'),
         (8 , '第八節'),
         (9 , '第九節')]
+        , render_kw={'class': 'form-control'}
     )
     sdate = DateField('日期')
     edate = DateField('日期')
@@ -24,16 +25,17 @@ class videoFilterUser(FlaskForm):
     lesson = SelectField(
         '節數',
         choices=[('第一節', 1), ('第二節', 2), ('第三節', 3),('第四節', 4),('第五節', 5),('第六節', 6),('第七節', 7),('第八節', 8),('第九節', 9)]
+        , render_kw={'class': 'form-control'}
     )
     sdate = DateField('日期')
     edate = DateField('日期')
 class pictureFilter(FlaskForm):
-    lastName = StringField('lastName', validators=[],render_kw={'placeholder': u'姓'})
-    firstName = StringField('firstName', validators=[],render_kw={'placeholder': u'名'})
+    lastName = StringField('lastName', validators=[],render_kw={'placeholder': u'姓' , 'class': 'form-control'})
+    firstName = StringField('firstName', validators=[],render_kw={'placeholder': u'名' , 'class': 'form-control'})
 
 class studentsFilter(FlaskForm):
-    lastName = StringField('lastName', validators=[],render_kw={'placeholder': u'姓'})
-    firstName = StringField('firstName', validators=[],render_kw={'placeholder': u'名'})
+    lastName = StringField('lastName', validators=[],render_kw={'placeholder': u'姓' , 'class': 'form-control'})
+    firstName = StringField('firstName', validators=[],render_kw={'placeholder': u'名' , 'class': 'form-control'})
 
 class classGroupFilter(FlaskForm):
     className = StringField('課程名稱' , render_kw={'class': 'form-control'})
