d73f2d31f6e666e9ca4f0fd30c61cb98015ce394
PUPU <majikkutori@gmail.com>
2019-10-24 17:57:27 +0800

    UI

--- flaskClass/ClassMemberForm.py
+++ flaskClass/ClassMemberForm.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Email
 import dbService.loginService as loginService
 
 class addClassMemberForm(FlaskForm):
-    account = StringField('學號', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(), Email()])
-    lastName =  StringField('姓', validators=[DataRequired()])
-    firstName =  StringField('名', validators=[DataRequired()])
+    account = StringField('學號', validators=[DataRequired()] , render_kw={'class': 'form-control'})
+    email = StringField('email', validators=[DataRequired(), Email()] , render_kw={'class': 'form-control'})
+    lastName =  StringField('姓', validators=[DataRequired()] , render_kw={'class': 'form-control'})
+    firstName =  StringField('名', validators=[DataRequired()] , render_kw={'class': 'form-control'})
