f25b238a17fbacedf62291f2ce813b89acea9b46
stanley_chen <stanley_chen@gss.com.tw>
2019-10-25 17:36:50 +0800

    len(isFaceExit)

--- recognitionApi.py
+++ recognitionApi.py
@@ -740,6 +740,7 @@ def upload():
             isRightFaceExit = getDataService.getFaceByType(current_user.id , 'right_face')
             isUpFaceExit = getDataService.getFaceByType(current_user.id , 'up_face')
             isDownFaceExit = getDataService.getFaceByType(current_user.id , 'down_face')
+            print(len(isFaceExit))
             return render_template('upload.html', form=uploadform ,
              isFaceExit = len(isFaceExit) > 0,
              isLeftFaceExit = len(isLeftFaceExit) > 0,
