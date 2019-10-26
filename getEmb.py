4c3358bb0e5bfd4422cdd2c4c1f5169a37242564
stanley_chen <stanley_chen@gss.com.tw>
2019-10-27 00:32:12 +0800

    all_obj

--- getEmb.py
+++ getEmb.py
@@ -1,4 +1,5 @@
 import os
+import scipy as sc
 from scipy import misc
 import facenet
 import numpy as np
@@ -6,26 +7,31 @@ import tensorflow as tf
 import dbService.insertDbService as service
 
 def getEmbList(modelPath , picturePathList):
+        sc.__version__
         with tf.Graph().as_default():
             config = tf.ConfigProto(allow_soft_placement=True)
             config.gpu_options.allow_growth = True 
             with tf.Session(config=config) as sess:
-                 # Load the model 
+                # Load the model 
                 model= modelPath
                 facenet.load_model(model)
+
                 # Get input and output tensors
                 images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
                 embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
                 phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
-                # 这里要改为自己emb_img文件夹的位置
+
                 # 讀取資料夾中的圖片產出 emb , emb 為 128 維
                 image=[]
                 nrof_images=0
+
                 for path in picturePathList:
+                    print(str(path))
                     img = misc.imread( path , mode='RGB')
                     prewhitened = facenet.prewhiten(img)
                     image.append(prewhitened)
                     nrof_images=nrof_images+1
+                
                 images=np.stack(image)
                 print(str(images.shape))
                 feed_dict = { images_placeholder: images, phase_train_placeholder:False }
