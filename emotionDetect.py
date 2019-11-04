48c3b8f0c93e2c1a9574640b6d4b7f98ae2543fb
yalin1997 <godjj1997@gmail.com>
2019-11-04 16:43:51 +0800

    Update emotionDetect.py

--- emotionDetect.py
+++ emotionDetect.py
@@ -14,27 +14,27 @@ EMOTIONS = ["angry" ,"disgust","scared", "happy", "sad", "surprised",
 
 def detectEmotion(face_image):
     # load model
-    #emotionGraph = tf.Graph()
-    #with emotionGraph.as_default():
-    configEmotion = tf.ConfigProto(allow_soft_placement=True)
-    configEmotion.gpu_options.allow_growth = True 
-    with tf.Session(config=configEmotion) as sessEmotion: 
-        K.set_session(sessEmotion)
-        emotion_classifier = load_model(emotion_model_path, compile=False)
-        emotion_classifier._make_predict_function()
-        print("+++++++++++++++++++++++ load emotion model finish +++++++++++++++++++++++")
+    emotionGraph = tf.Graph()
+    with emotionGraph.as_default():
+        configEmotion = tf.ConfigProto(allow_soft_placement=True)
+        configEmotion.gpu_options.allow_growth = True 
+        with tf.Session(config=configEmotion) as sessEmotion: 
+            K.set_session(sessEmotion)
+            emotion_classifier = load_model(emotion_model_path, compile=False)
+            emotion_classifier._make_predict_function()
+            print("+++++++++++++++++++++++ load emotion model finish +++++++++++++++++++++++")
 
-        face_image = cv2.resize(face_image, (64, 64))
-        roi = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY)
-        roi = roi.astype("float") / 255.0
-        roi = img_to_array(roi)
-        roi = np.expand_dims(roi, axis=0)
-        
-        print("++++++++++++++++ start predict emotion +++++++++++++++++++")
-        preds = emotion_classifier.predict(roi)[0]
-        emotion_probability = np.max(preds)
-        label = EMOTIONS[preds.argmax()]
-        print("!!!!!!!!!!!!!!!!!!!!!!!! Label : " + str(label))
+            face_image = cv2.resize(face_image, (64, 64))
+            roi = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY)
+            roi = roi.astype("float") / 255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi, axis=0)
+            
+            print("++++++++++++++++ start predict emotion +++++++++++++++++++")
+            preds = emotion_classifier.predict(roi)[0]
+            emotion_probability = np.max(preds)
+            label = EMOTIONS[preds.argmax()]
+            print("!!!!!!!!!!!!!!!!!!!!!!!! Label : " + str(label))
     return label
 
 
