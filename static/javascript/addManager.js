60c1b93b01f3c623c728698c5ec7c825fedc49e4
yalin1997 <godjj1997@gmail.com>
2019-11-07 18:07:56 +0800

    Update addManager.js

--- static/javascript/addManager.js
+++ static/javascript/addManager.js
@@ -1,18 +1,18 @@
 function sentManagerData(){
     var formData = new FormData($('#addManagerForm')[0]);
     $.ajax({
-        url: '/addManager',
         type: 'POST',
-        cache: false,
-        data:formData,
-        processData: false,
-        contentType: false
-    }).done(function(data) {
-        if (data.result == true){
-            alert('新增完成!');
-        }
-        else{
-            alert(data.result);
-        }
-    }).fail(function(res) { alert("發生異常，請檢察網路狀況!");});
+        url: '/addManager',
+        data: formData,
+        success: function(data){
+            if (data.result == true){
+                alert('新增完成!');
+            }
+            else{
+                alert(data.result)
+            }
+        },
+        contentType: "application/json",
+        dataType: 'json'
+    });
 }
