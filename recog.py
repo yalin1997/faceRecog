29ab7cfac5613ace9df60ccf8aa074a34375db6d
yalin1997 <godjj1997@gmail.com>
2020-03-04 22:36:10 +0800

    Update recog.py

--- recog.py
+++ recog.py
@@ -148,13 +148,26 @@ def main(videoId , uploadFile , fileName , emdList , modelPath , all_name , date
                                     faceVideoId = insertService.InsertFocusVideoInfo(date , classNo , classId ,'/upload/' + faceVideoFileName , faceCoverUrl , 1 , faceVideoFileName , faceVideoUrl , str(fin_obj[rec_position]))
                                     insertService.insertRecogedUser(videoId , int(str(fin_obj[rec_position]).split('_')[1]))
                                     insertService.insertRecogedUser(faceVideoId , int(str(fin_obj[rec_position]).split('_')[1]))
-                                print(str(bounding_box[rec_position,1]))
-                                print(str(bounding_box[rec_position,3]))
-                                print(str(bounding_box[rec_position,3] - bounding_box[rec_position,1]))
-                                facePicFrame = frame[bounding_box[rec_position,1]:bounding_box[rec_position,3],bounding_box[rec_position,0]:bounding_box[rec_position,2]]
+
+                                # 調整抓取的範圍
+                                xBound = bounding_box[rec_position,3] - bounding_box[rec_position,1]
+                                yBound = bounding_box[rec_position,0] - bounding_box[rec_position,2]
+                                if bounding_box[rec_position,1] - (xBound/3) >= 0:
+                                    xPos1 = bounding_box[rec_position,1] - (xBound/3)
+                                else:
+                                    xPos1 = 0
+                                if bounding_box[rec_position,3] + (xBound/3) <= np.size(frame , 0):
+                                    xPos2 = bounding_box[rec_position,3] + (xBound/3)
+                                else:
+                                    xPos2 = np.size(frame , 0)
+                                if bounding_box[rec_position,2] + yBound <= np.size(frame , 1):
+                                    yPos = bounding_box[rec_position,2] + yBound
+                                else:
+                                    yPos = np.size(frame , 1)
+
+                                facePicFrame = frame[ xPos1 : xPos2 , bounding_box[rec_position,0]:yPos]
                                 cv2.imwrite(faceCoverPath ,facePicFrame)
                                 emotion = emotionDetect.detectEmotion(facePicFrame)
-                                print(str(type(facePicFrame)))
                                 # azure face cognition
                                 #detected_emotion = azureFaceDetect.detectFace(facePicFrame)
                                 #print("id:"+detected_emotion.face_id)
