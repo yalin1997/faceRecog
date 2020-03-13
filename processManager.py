d7a1c452de006654a6430446897de8fbaf74b084
stanley_chen <stanley_chen@gss.com.tw>
2020-03-13 23:22:51 +0800

    process manager

---
+++ processManager.py
@@ -0,0 +1,90 @@
+from multiprocessing.managers import BaseManager
+from multiprocessing import RLock
+
+
+MANAGER_PORT = 6000
+MANAGER_DOMAIN = '0.0.0.0'
+MANAGER_AUTH_KEY = 'nknuwe310a'
+
+class QueueItem():
+    def __init__(self, ):
+        self.items = Queue()
+
+    def set(self, value):
+        self.items.put(value)
+
+    def get(self):
+        if self.items.not_empty:
+            value = self.items.get()
+        else:
+            value = None
+        return value
+
+class isRuning():
+    def __init__(self , ):
+        self.runing = False
+    def set(self):
+        if this.runing :
+            self.runing = False
+        else:
+            self.runing = True
+
+    def get(self):
+        return self.runing
+        
+
+d = QueueItem()
+lock = RLock()
+runing = isRuning()
+
+BaseManager.register('queue', callable=lambda: d)
+BaseManager.register('open_qq_login_lock', callable=lambda: lock)
+BaseManager.register('isRuning' , callable=lambda: runing)
+class ManagerServer():
+    
+    # multiprocess Manager 服務Class
+    
+    def __init__(self, domain, port, auth_key):
+        self.domain = domain
+        self.port = port
+        self.auth_key = auth_key
+
+    def start_manager_server(self):
+        self.queue_manager = BaseManager(address=('', self.port), authkey=self.auth_key)
+        # self.dict = self.queue_manager.dict()
+        self.server = self.queue_manager.get_server()
+
+    def run(self):
+        self.start_manager_server()
+        self.server.serve_forever()
+
+    def stop(self):
+        self.server.shutdown()
+        self.is_stop = 1
+
+class ManagerClient():
+# Manager server client
+    def __init__(self, domain, port, auth_key):
+        self.domain = domain
+        self.port = port
+        self.auth_key = auth_key
+        # self.get_share_dict()
+        self.info_manager = BaseManager(address=(self.domain, self.port), authkey=self.auth_key)
+        self.info_manager.connect()
+
+    def getQueue(self):
+        # self.dict = m.dict()
+        self.queue = self.info_manager.queue()
+        return self.queue
+
+    def get_open_qq_login_lock(self):
+        self.open_qq_login_lock = self.info_manager.open_qq_login_lock()
+        return self.open_qq_login_lock
+
+    def getIsRuning(self):
+        return self.info_manager.isRuning()
+
+if __name__ == '__main__':
+    manager_server = ManagerServer(MANAGER_DOMAIN, MANAGER_PORT, MANAGER_AUTH_KEY)
+    manager_server.run()
+
--- recognitionApi.py
+++ recognitionApi.py
@@ -35,6 +35,7 @@ import os.path
 import uuid
 import sys
 import subprocess
+import processManager
 
 app = flask.Flask(__name__)
 
@@ -58,6 +59,8 @@ app.config["JSON_AS_ASCII"] = False
 
 model_Path = getDataService.getModelPath() # 取模型路徑 tuple list
 
+# get general var
+manager_client = processManager.ManagerClient(processManager.MANAGER_DOMAIN, processManager.MANAGER_PORT, processManager.MANAGER_AUTH_KEY)
 
 # flask Bootstrap randerer
 bootstrap = Bootstrap(app)
@@ -505,20 +508,29 @@ def videoManage():
 @app.route('/videoRecog' , methods = ['POST'])
 @login_required
 def videoRecog():
-    videoId = request.get_json(force=True)["videoId"]
-    if videoId and current_user.permission == 'manager':
-        result = getDataService.getVideoById(videoId)
-        classId = int(result[0][3])
-        memberList = getDataService.getStudentsPicture(classId)
-        if len(memberList) > 0:
-            '''with ThreadPoolExecutor() as executor: 
-                executor.submit(recogTask, videoId , str(result[0][2]) , str(result[0][8]) , result[0][5] , result[0][7] , classId )'''
-            recogTask(videoId , str(result[0][2]) , str(result[0][8]) , str(result[0][5]) , result[0][7] , classId)
-            return jsonify({'result':True})
+    runing = manager_client.getIsRuning()
+    lock = manager_client.get_open_qq_login_lock()
+    if not runing.get():
+        lock.acquire()
+        runing.set()
+        lock.release()
+        videoId = request.get_json(force=True)["videoId"]
+        if videoId and current_user.permission == 'manager':
+            result = getDataService.getVideoById(videoId)
+            classId = int(result[0][3])
+            memberList = getDataService.getStudentsPicture(classId)
+            if len(memberList) > 0:
+                '''with ThreadPoolExecutor() as executor: 
+                    executor.submit(recogTask, videoId , str(result[0][2]) , str(result[0][8]) , result[0][5] , result[0][7] , classId )'''
+                recogTask(videoId , str(result[0][2]) , str(result[0][8]) , str(result[0][5]) , result[0][7] , classId)
+                return jsonify({'result':True})
+            else:
+                return jsonify({'result':"沒有辨識目標，請加入學生"})
         else:
-            return jsonify({'result':"沒有辨識目標，請加入學生"})
+            return jsonify({'result':"權限不足"})
     else:
-        return jsonify({'result':"權限不足"})
+        return jsonify({'result':"尚有工作進行中，請完成後再試"})
+
 
 def recogTask(videoId ,filename, filePath , date , classNo, classId ):
     subprocess.Popen(["python","/home/nknu/文件/faceRecog/recog.py" , str(videoId) , filePath , filename , date , str(classNo) , str(classId)])
