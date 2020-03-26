de1d6be082b79e69fe8d35d200fba420a21bea63
stanley_chen <stanley_chen@gss.com.tw>
2020-03-26 11:10:13 +0800

    串流測試

--- recognitionApi.py
+++ recognitionApi.py
@@ -783,10 +783,11 @@ def faceLocateTask( face , leftFace , rightFace , upFace , downFace ):
 @login_required
 def uploaded_file(filename):
     if allowed_picture(filename) :
-        print(filename)
         return send_from_directory(app.config['UPLOAD_FOLDER']+embPath,filename)
     elif allowed_video(filename):
-        return send_from_directory(app.config['UPLOAD_FOLDER']+videoPath,filename)
+        path = os.path.join(app.config['UPLOAD_FOLDER']+videoPath , filename)
+        start, end = get_range(request)
+        return partial_response(path, start, end)
     else:
         return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
@@ -795,12 +796,67 @@ def uploaded_file(filename):
 @login_required
 def uploaded_file(filename):
     if allowed_picture(filename) :
-        print(filename)
         return send_from_directory(app.config['UPLOAD_FOLDER']+embPath,filename , as_attachment=True)
     elif allowed_video(filename):
         return send_from_directory(app.config['UPLOAD_FOLDER']+videoPath,filename , as_attachment=True)
     else:
         return send_from_directory(app.config['UPLOAD_FOLDER'],filename , as_attachment=True)
+
+def partial_response(path, start, end=None):
+    LOG.info('Requested: %s, %s', start, end)
+    file_size = os.path.getsize(path)
+
+    # Determine (end, length)
+    if end is None:
+        end = start + BUFF_SIZE - 1
+    end = min(end, file_size - 1)
+    end = min(end, start + BUFF_SIZE - 1)
+    length = end - start + 1
+
+    # Read file
+    with open(path, 'rb') as fd:
+        fd.seek(start)
+        bytes = fd.read(length)
+    assert len(bytes) == length
+
+    response = Response(
+        bytes,
+        206,
+        mimetype=mimetypes.guess_type(path)[0],
+        direct_passthrough=True,
+    )
+    response.headers.add(
+        'Content-Range', 'bytes {0}-{1}/{2}'.format(
+            start, end, file_size,
+        ),
+    )
+    response.headers.add(
+        'Accept-Ranges', 'bytes'
+    )
+    LOG.info('Response: %s', response)
+    LOG.info('Response: %s', response.headers)
+    return response
+
+
+def get_range(request):
+    range = request.headers.get('Range')
+    LOG.info('Requested: %s', range)
+    m = re.match('bytes=(?P<start>\d+)-(?P<end>\d+)?', range)
+    if m:
+        start = m.group('start')
+        end = m.group('end')
+        start = int(start)
+        if end is not None:
+            end = int(end)
+        return start, end
+    else:
+        return 0, None
+    
+@app.route('/video/<filename>'))
+def video(filename):
+    path = os.path.join(app.config['UPLOAD_FOLDER']+videoPath , filename)
+    start, end = get_range(request)
+    return partial_response(path, start, end)
 
 
 @app.route('/upload/others/<filename>')
